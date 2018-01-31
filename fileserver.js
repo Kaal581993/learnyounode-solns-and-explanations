3a3edf374d2311e488e70edadf05578415db7f8a
Viral Prajapati (a.k.a KAAL) <viral.prajapati.1993@gmail.com>
2018-01-31 13:50:38 +0530

    Add files via upload

---
+++ fileserver.js
@@ -0,0 +1,39 @@
+/*
+  ## HTTP FILE SERVER (Exercise 11 of 13):
+
+  Write an HTTP server that serves the same text file for each request it
+  receives.
+
+  Your server should listen on the port provided by the first argument to
+  your program.
+
+  You will be provided with the location of the file to serve as the second
+  command-line argument. You must use the fs.createReadStream() method to
+  stream the file contents to the response.
+
+ ──────────────────────────────────────────────────────────*/
+ var http = require('http'),
+     fs = require('fs'),
+     port = process.argv[2],
+     path = process.argv[3];
+
+ var server = http.createServer(function(request, response){
+     var file = fs.createReadStream(path);
+     file.pipe(response);
+ });
+
+ server.listen(port);
+ /*
+ Official Soluntion:
+
+ var http = require('http')
+    var fs = require('fs')
+
+    var server = http.createServer(function (req, res) {
+      res.writeHead(200, { 'content-type': 'text/plain' })
+
+      fs.createReadStream(process.argv[3]).pipe(res)
+    })
+
+    server.listen(Number(process.argv[2]))
+ */
